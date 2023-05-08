a7058d5f6d6a8d43d2264c3c66b06da9ff824c1a
muhammaduzair00290 <muhammaduzair26122001@gmail.com>
2023-05-08 03:08:20 -0700

    p2 done

--- 7mayassignment/Program.cs
+++ 7mayassignment/Program.cs
@@ -1,42 +1,42 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("assignment using if and switch statement");
 // using if else conditions 
-//Console.Write(" enter the no to check the day using if : ");
-//int a =Convert.ToInt32(Console.ReadLine());
+Console.Write(" enter the no to check the day using if : ");
+int a = Convert.ToInt32(Console.ReadLine());
 
 
-//if (a == 1)
-//{
-//    Console.WriteLine(" Monday");
-//}
-// else if (a == 2)
-//{
-//    Console.WriteLine(" Tuesday");
-//}
-// else if (a == 3)
-//{
-//    Console.WriteLine(" Wednesday");
-//}
-// else if (a == 4)
-//{
-//    Console.WriteLine(" Thursday");
-//}
-//else if (a == 5)
-//{
-//    Console.WriteLine(" Friday");
-//}
-//else if (a == 6)
-//{
-//    Console.WriteLine(" Saturday");
-//}
-//else if (a == 7)
-//{
-//    Console.WriteLine(" Sunday");
-//}
-//else
-//{
-//    Console.WriteLine(" invalid input");
-//}
+if (a == 1)
+{
+    Console.WriteLine(" Monday");
+}
+else if (a == 2)
+{
+    Console.WriteLine(" Tuesday");
+}
+else if (a == 3)
+{
+    Console.WriteLine(" Wednesday");
+}
+else if (a == 4)
+{
+    Console.WriteLine(" Thursday");
+}
+else if (a == 5)
+{
+    Console.WriteLine(" Friday");
+}
+else if (a == 6)
+{
+    Console.WriteLine(" Saturday");
+}
+else if (a == 7)
+{
+    Console.WriteLine(" Sunday");
+}
+else
+{
+    Console.WriteLine(" invalid input");
+}
 //using switch statement :
 Console.Write(" enter the no again to check day using switch statement:  ");
 int b = Convert.ToInt32(Console.ReadLine());
--- 7mayassignmentp2/Program.cs
+++ 7mayassignmentp2/Program.cs
@@ -1,3 +1,71 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("assignment program 2 using if and switch");
 
+Console.Write(" enter the no to check the flavor using if : ");
+int flavor  = Convert.ToInt32(Console.ReadLine());
+
+
+if (flavor == 1)
+{
+    Console.WriteLine("Chicken Fajita ");
+}
+else if (flavor == 2)
+{
+    Console.WriteLine(" Chicken Tikka");
+}
+else if (flavor == 3)
+{
+    Console.WriteLine("BBQ Pizza ");
+}
+else if (flavor == 4)
+{
+    Console.WriteLine(" Pepperoni");
+}
+else if (flavor == 5)
+{
+    Console.WriteLine(" Malai Boti");
+}
+else if (flavor == 6)
+{
+    Console.WriteLine(" Veggie pizza");
+}
+else if (flavor == 7)
+{
+    Console.WriteLine("Hot & Spicy ");
+}
+else
+{
+    Console.WriteLine(" required flavor is not available ");
+}
+//using switch statement :
+Console.Write(" enter the no again to check flavor  using switch statement:  ");
+int b = Convert.ToInt32(Console.ReadLine());
+switch (b)
+{
+    case 1:
+        Console.WriteLine(" Chicken Fajita");
+        break;
+    case 2:
+        Console.WriteLine("Chicken Tikka  ");
+        break;
+    case 3:
+        Console.WriteLine(" BBQ Pizza");
+        break;
+    case 4:
+        Console.WriteLine(" Pepperoni");
+        break;
+    case 5:
+        Console.WriteLine(" Malai Boti");
+        break;
+    case 6:
+        Console.WriteLine(" Veggie pizza");
+        break;
+    case 7:
+        Console.WriteLine("Hot & Spicy");
+        break;
+    default:
+        Console.WriteLine(" flavor is not available ");
+        break;
+}
+
+
