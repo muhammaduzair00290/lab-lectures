3905c19fe5a4526babe002df0037908408529da6
muhammaduzair00290 <muhammaduzair26122001@gmail.com>
2023-05-09 04:14:09 -0700

    9 may lec an dassignment

--- 9may/Program.cs
+++ 9may/Program.cs
@@ -1,7 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 //Console.WriteLine("Hello, World!");
 //// substring 
-//// Full name
+// Full name
 //string name = "uzair ali";
 
 //// Location of the letter D
@@ -12,7 +12,7 @@
 
 //// Print the result
 //Console.WriteLine(lastName);
-//// strings methods 
+// strings methods 
 //string? name1 = " uzair tariq";
 //string? name2 = " software developer";
 //Console.WriteLine(name1.ToUpper());
@@ -32,12 +32,89 @@
 //    i--;
 //}
 // do while loop
-int b = 1;
-do
-{
-    Console.WriteLine(b);
-    b++;
-}
-while (b < 15);
+//int b = 1;
+//do
+//{
+//    Console.WriteLine(b);
+//    b++;
+//}
+//while (b < 15);
+//int a = 1;
+//int b = 3;
+//while (a <= 10)
+//{
 
+//    Console.WriteLine(b + " * " + a + " = " + b * a);
+
+//    a++;
+
+//}
+//int a = 6;//starting value
+//int b = 8;//ending value
+//while (a <= b)
+//{
+//    int i = 1;
+//    while (i <= 10)
+//    {
+//        Console.WriteLine($"{a} x {i} = {a * i}");
+//        i++;
+//    }
+//    a++;
+//}
+//    Console.WriteLine(" enter the starting number ");
+//int a =Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine(" enter the ending value ");
+//int b =Convert.ToInt32(Console.ReadLine());
+//while (a<=b)
+//{
+//    int c = 1;
+//    while (c<=10)
+//    {
+//        Console.WriteLine($"{a}*{c} ={a*c} ");
+//        c++;
+//    }
+//    a++;
+//} 
+//Console.WriteLine(" enter the no to check ");
+//int num = Convert.ToInt32(Console.ReadLine());
+
+//if (num % 2 == 0)
+//{
+//    Console.WriteLine($"{num} is even ");
+
+//}
+//else
+//{
+//    Console.WriteLine(" the no is odd ");
+//    }
+
+Console.Write("Enter any number: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+int f = 0;
+int i = 2;
+while (i <= num1 / 2)
+{
+    if (num1 % i == 0)
+    {
+        f = 1;
+        break;
+    }
+    i++;
+}
+if (f == 0)
+    Console.WriteLine(num1 + " is a Prime Number");
+else
+    Console.WriteLine(num1 + " is not a Prime Number");
+
+//int k = 3 / 2; Console.WriteLine(k);
+//int num2 =Convert.ToInt32(Console.ReadLine());
+//if (num2 % num2 == 0 && num2 % 2 == 1 && num2 % 1 ==num2) 
+//{
+//    Console.WriteLine(" the no is prime ");
+//}
+//else
+//{
+//    Console.WriteLine(" no is not prime ");
+//}
 
