63cc2eff97a87a4ac006f85be7170bb5e5a1542f
muhammaduzair00290 <muhammaduzair26122001@gmail.com>
2023-05-09 00:36:09 -0700

    9 may lec

---
+++ 9may/Program.cs
@@ -0,0 +1,43 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+//// substring 
+//// Full name
+//string name = "uzair ali";
+
+//// Location of the letter D
+//int charPos = name.IndexOf(" ");
+
+//// Get last name
+//string lastName = name.Substring(charPos);
+
+//// Print the result
+//Console.WriteLine(lastName);
+//// strings methods 
+//string? name1 = " uzair tariq";
+//string? name2 = " software developer";
+//Console.WriteLine(name1.ToUpper());
+//Console.WriteLine(name1.ToLower());
+//Console.WriteLine(name1[4]);
+//Console.WriteLine(name1.IndexOf("z"));
+//Console.WriteLine(string.Concat(name1, name2));
+//Console.WriteLine(" hello \" uzair \" bhaii");
+//Console.WriteLine(" hello \t bhaii");
+//Console.WriteLine(" hello \n bhaii");
+//Console.WriteLine(" hello \b bhaii");
+// while loop :
+//int i = 10;
+//while (i >= 5)
+//{
+//    Console.WriteLine(i);
+//    i--;
+//}
+// do while loop
+int b = 1;
+do
+{
+    Console.WriteLine(b);
+    b++;
+}
+while (b < 15);
+
+
